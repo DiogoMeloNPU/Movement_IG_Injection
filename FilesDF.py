6d39107fedc830abbe6d18d330131cc3fe348e9e
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-11-15 16:31:54 +0000

    Now this code builds the same FilesDF, however it uses the .npy file produced in 'SelectedVideos4Analysis.py'

--- FilesDF.py
+++ FilesDF.py
@@ -1,28 +1,73 @@
 import pandas as pd
 import numpy as np
 import os
+import cv2
 
 #Create a dataframe containing the paths for the session videos and DLC files
 
 #define the column labels
-columns = ['Imaging', 'MiceID', 'Reinforcer', 'SessionDay', 'VideoPath', 'FilteredDLCPath', 'FrameSessionOnset']
+columns = ['Imaging', 'MiceID', 'Reinforcer', 'SessionDay',
+           'VideoPath', 'VideoAqRate', 'FrameSessionOnset', 'FilteredDLCPath']
 
 #create the df
 FilesDF = pd.DataFrame(columns = columns)
 
-#folder containing all files used for training the DLC network
-path_files = "C:\\Users\\user\\Desktop\\DLC_postingestiveReinforcers-Diogo-Melo-2022-11-04\\videos"
+#load the video paths to create the dataframe
+videoPathsAndFPS_path = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\videoPathsAndFPS.npy"
+videoPathsAndFPS = np.load(videoPathsAndFPS_path)
 
-#Now, fill the dataframe using the files stored in path_files
-for path, subdirs, files in os.walk(path_files):
-    for name in files:
-        if (name.endswith('.mp4') or name.endswith('.avi')) and 'labeled' not in name:
-            #use the video name to fill the first columns of the dataframe
-            currentRowValues = {'Imaging':name.split('_')[0], 'MiceID':name.split('_')[1], 'Reinforcer':name.split('_')[2], 'SessionDay':name.split('_')[3][3:], 
-            'VideoPath':os.path.join(path, name), 'FilteredDLCPath':np.nan, 'FrameSessionOnset':np.nan}
-            FilesDF = FilesDF.append(currentRowValues, ignore_index=True)
+#The following loop hasn't been tested for possible bugs. Be mindful that the multiple if statements to correct for files that were badly labeled may need to be updated for future use.
+#Now, fill the dataframe using the info present in each video file name
+for i in range(videoPathsAndFPS.shape[1]):
+    file_name = videoPathsAndFPS[0, i].split('\\')[-1]
+    #get the imaging region (either SN or VTA)
+    imaging_region = file_name.split('_')[0]
+    #get the MiceID
+    MiceID = file_name.split('_')[1]
+    #get the session day
+    sessionDay = file_name.split('_')[3][3:]
+    #correct the cases in which the file name doesn't start with the region being imaged
+    if not file_name.startswith('SN') and not file_name.startswith('VTA'):
+        prefix = videoPathsAndFPS[0, i].split('\\')[4]
+        if prefix == 'SNc':
+            imaging_region = 'SN'
+        elif prefix == 'VTA':
+            imaging_region = 'VTA'
+        MiceID = file_name.split('_')[0]
+        sessionDay = -1 #meaning I can't get that information from the file name
+    #get the Reinforcer that was injected IG
+    reinforcer = file_name.split('_')[2]
+    #correct 'COIL'/'CORNOIL'
+    if reinforcer == 'COIL':
+        reinforcer = 'CORNOIL'
+    #correct cases in which a date was incorporated in the name AND the beggining of the file name corresponds to the imaging region
+    if reinforcer.isdigit():
+        reinforcer = file_name.split('_')[3]
+        sessionDay = file_name.split('_')[4][3:]
+    #correct for specific error in building the file name 'M46682SUCROSE' (miceID and reinforcer are merged)
+    if len(MiceID) > 6:
+        sessionDay = reinforcer[3:]
+        MiceID = MiceID[:6]
+        reinforcer = file_name.split('_')[1][6:]
+    #correct the sessionday in the case of a baseline session
+    if reinforcer == 'BASELINE':
+        sessionDay = 0
+    #account for the names that may display a different pattern 
+    if file_name:
+        #fill the dataframe with the exception of the path of the filtered DLC coordinate predictions
+        currentRowValues = {'Imaging': imaging_region, 'MiceID': MiceID, 'Reinforcer': reinforcer, 'SessionDay': sessionDay,
+                            'VideoPath': videoPathsAndFPS[0, i], 'VideoAqRate': videoPathsAndFPS[1, i], 'FrameSessionOnset': np.nan, 'FilteredDLCPath': np.nan}
+    #add the new row to the dataframe
+    FilesDF = FilesDF.append(currentRowValues, ignore_index=True)
 
-#Match the path of the filtered DLC predictions to the respective row in the df
+#delete if unnecessary
+'''#folder containing all the files for the analysis
+drive_files_path_SNc = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\SNc"
+drive_files_path_VTA = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\VTA"
+drive_files_path_total = [drive_files_path_SNc, drive_files_path_VTA]'''
+
+#run this after the DLC analysis is complete
+'''#Match the path of the filtered DLC predictions to the respective row in the df
 for path, subdirs, files in os.walk(path_files):
     for name in files:
         if name.endswith('500000_filtered.csv'):
@@ -30,13 +75,13 @@ for path, subdirs, files in os.walk(path_files):
             currentDLCFile = [name.split('_')[0], name.split('_')[1], name.split('_')[2], name.split('_')[3][3:]]
             match = FilesDF.loc[(FilesDF['Imaging'] == currentDLCFile[0]) & (FilesDF['MiceID'] == currentDLCFile[1]) & (FilesDF['Reinforcer'] == currentDLCFile[2]) & (FilesDF['SessionDay'] == currentDLCFile[3])]
             if len(match) == 1:
-                FilesDF['FilteredDLCPath'][match.index[0]] = os.path.join(path, name)
+                FilesDF['FilteredDLCPath'][match.index[0]] = os.path.join(path, name)'''
 
 #show the df
 print(FilesDF)
 
 #save the df in google drive as pkl
-path2saveFilesDF = "E:\\My Drive\\MovementIGInjectionFilesDF.pkl"
+path2saveFilesDF = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\MovementIGInjectionFilesDF.pkl"
 FilesDF.to_pickle(path2saveFilesDF)
 
 #if you want, you an also save it in other formats (such as csv for easier direct search - not recommended)
