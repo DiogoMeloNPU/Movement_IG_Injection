662ba9cebc42bb0735ab82f413f8d69b053b71ea
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-11-15 14:28:47 +0000

    Adatped the code to also save hte video aq rate in the .npy variable

--- SelectVideos4Analysis.py
+++ SelectVideos4Analysis.py
@@ -20,11 +20,12 @@ def video_duration_min(video_path):
         duration_sec = frame_count/videofps
         duration_min = int(duration_sec/60)
 
-    return duration_min
+    return duration_min, videofps
 
 #create an array to save the video file paths
 paths2inputDLCanalysis = []
-
+#create an array to save the video aq rate
+currentAqRate = []
 #store the files in an array
 for imaging_region in drive_files_path_total:
     for path, subdirs, files in os.walk(imaging_region):
@@ -34,20 +35,24 @@ for imaging_region in drive_files_path_total:
                 temp_path = temp_path.split('\\')
                 currentpath = '\\'.join(temp_path)
                 #get the duration of the video in minutes
-                duration_min = video_duration_min(currentpath)
+                duration_min, videofps = video_duration_min(currentpath)
                 if duration_min > 20:
                     paths2inputDLCanalysis.append(currentpath)
+                    currentAqRate.append(videofps)
                     print('The following video will be analyzed with DLC: {}'.format(currentpath))
                 else:
                     print('The current video is 0 frames long and should be discarded: {}'.format(currentpath))
 
-#show the complete array
-print(paths2inputDLCanalysis)
+#create a 2D array to save the video aq rate and the video files paths
+videoPathsAndFPS = [paths2inputDLCanalysis, currentAqRate]
+
+#show the 2D array
+print(videoPathsAndFPS)
 
 #convert to np.array and save the variable as a .npy
-paths2inputDLCanalysis = np.array(paths2inputDLCanalysis)
-save_here = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\paths2inputDLCanalysis.npy"
-np.save(save_here, paths2inputDLCanalysis, allow_pickle=True)
+videoPathsAndFPS = np.array(videoPathsAndFPS)
+save_here = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\videoPathsAndFPS.npy"
+np.save(save_here, videoPathsAndFPS, allow_pickle=True)
 
-#how many videos will vbe analyzed
+#how many videos will be analyzed
 print(len(paths2inputDLCanalysis))
