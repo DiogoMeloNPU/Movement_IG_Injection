94f84bbb0059a47c0f99d9a4331d3b15cc736aba
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-11-16 15:43:19 +0000

    Save the number of the session onset frame in a .npy

--- NumFrameSessionOnset.py
+++ NumFrameSessionOnset.py
@@ -23,15 +23,23 @@ def rewind_key_action():
     #resets the current frame to one second earlier
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_counter)
 
+#implement a way to save the current state of the sessionOnsets array as .npy and restart where I left (so that if something wrong happens during identification of session
+# onset frames, I don't lose the info on the videos I had already checked)
+
+#the implementation of the onset_added variable only works if the videos are all closed with the specified 'character'. Otherwise, if the loop doesn't run until the end, 
+#...the size of the array will not match the number of videos
+
 #create an array to save all the frame numbers corresponding to the session onset
 sessionOnsets = []
 for video_path in FilesDF['VideoPath']:
+    #create a variable to check if a frame of session onset was identified for the current video (in each iteration)
+    onset_added = False
     cap = cv2.VideoCapture(video_path)
     while cap.isOpened():
         # Read video capture
         ret, frame = cap.read()
         # Display each frame
-        cv2.putText(frame, video_path.split('\\')[-1], (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(frame, video_path.split('\\')[-1], (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
         cv2.imshow("video", frame)
         # show one frame at a time
         key = cv2.waitKey(0)
@@ -44,12 +52,27 @@ for video_path in FilesDF['VideoPath']:
         if key == ord('s'):
             numFrameSessionOnset = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
             sessionOnsets.append(numFrameSessionOnset)
+            onset_added = True
             break
         # Quit when 'q' is pressed
         if key == ord('q'):
+            if onset_added == False:
+                sessionOnsets.append(-1)
             break
 
+#correct the array for the cases in which the user just closes the loop before it running completely
+videos_not_assessed = -1*np.ones(len(FilesDF)-len(sessionOnsets))
+print(videos_not_assessed)
+sessionOnsets = np.concatenate(sessionOnsets, videos_not_assessed)
+
+#print the values that will be stored as .npy
 print(sessionOnsets)
+
+#before saving, complete the array (until its length equals the number of videos used) with -1, to signal for 'num of onset frame not obtained yet'
+#save as .npy and filll the 'unfilled' with -1
+sessionOnsets = np.array(sessionOnsets)
+path2saveNPY = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\numFrameSessionOnset.npy"
+np.save(path2saveNPY, sessionOnsets)
 
 #If you reached this part of the code you have an array with the number of the frames corresponding to the beggining of each session
 #So, it is time to place those values in the proper column of FilesDF and store the file again as pkl
@@ -59,4 +82,5 @@ FilesDF['FrameSessionOnset'] = sessionOnsets
 print(FilesDF)
 
 #save the updated file as pkl
-#FilesDF.to_pickle(FilesDF_path)
+if -1 not in sessionOnsets:
+    FilesDF.to_pickle(FilesDF_path)
