8b1c6ebe675f915acbddb426f40ecb8e4618d1e4
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-11-18 16:53:41 +0000

    Created a file to bin the velocity signals (for plotting)

---
+++ createBinnedData.py
@@ -0,0 +1,7 @@
+#this module is designed to bin the data from the velocity dataframes
+#the resulting files, which will be .npy (containing the binned velocities) will be used for the final plot
+
+#open the FilesDF.pkl that was created in FilesDF.py
+FilesDFpath = "E:\\.shortcut-targets-by-id\\1un_-G2CqE1eg6sx5KdpwrQRyPBJ1REFA\\All_video_data_baseline\\MovementIGInjectionFilesDF.pkl"
+FilesDF = pd.read_pickle(FilesDFpath)
+
